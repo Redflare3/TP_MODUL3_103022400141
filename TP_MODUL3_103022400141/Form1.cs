ab4d411bb14c05849d4c4ce41164a950d1b7f9fb
Solar <redflare31138@gmail.com>
2026-03-08 13:44:10 +0700

    Menambahkan tampilan GUI kalkulator

--- TP_MODUL3_103022400141/Form1.cs
+++ TP_MODUL3_103022400141/Form1.cs
@@ -6,5 +6,15 @@ namespace TP_MODUL3_103022400141
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
